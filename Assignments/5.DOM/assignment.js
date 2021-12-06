0e08b34b8c77d6af41550a502854dd40434dfab7
Rajashekar G Naidu <raju10naidu@gmail.com>
2021-12-06 17:55:58 +0530

    assignment 4 completed

---
+++ Assignments/5.DOM/assignment.js
@@ -0,0 +1,7 @@
+document.getElementById('task-1').style.backgroundColor = 'black';
+document.querySelector('li').style.color = 'white';
+
+// document.querySelector('title').textContent = "Assignment - Solved!";
+document.head.querySelector('title').textContent = "Assignment - Solved!";
+
+document.querySelector('h1').textContent = "Assignment - Solved!";
